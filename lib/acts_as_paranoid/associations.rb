6d76b2cf20d3a2029afa6d69465d7cd7c50d021e
Alex Parker <misterparker@gmail.com>
2013-01-06 10:04:03 -0700

    Trial adding has_many associations

--- lib/acts_as_paranoid/associations.rb
+++ lib/acts_as_paranoid/associations.rb
@@ -29,6 +29,25 @@ module ActsAsParanoid
 
         result
       end
+
+      def has_many_with_deleted(target, options = {})
+        with_deleted = options.delete(:with_deleted)
+        result = has_many_without_deleted(target, options)
+        if with_deleted
+          result.options[:with_deleted] = with_deleted
+          unless method_defined? "#{target}_with_unscoped"
+            class_eval <<-RUBY, __FILE__, __LINE__
+              def #{target}_with_unscoped(*args)
+                association = association(:#{target})
+                return nil if association.options[:polymorphic] && association.klass.nil?
+                return #{target}_without_unscoped(*args) unless association.klass.paranoid?
+                association.klass.with_deleted.scoping { #{target}_without_unscoped(*args) }
+              end
+              alias_method_chain :#{target}, :unscoped
+            RUBY
+          end
+        end
+      end
     end
   end
 end
